52b5ead2704e5aee0880738814da3656cc57deb7
אוהד סעדיה <ohad24333@gmail.com>
2022-03-22 23:39:19 +0200

    Practice styles and classes HWS

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -8,20 +8,41 @@ import type { User } from '../interface/User';
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.color = "blue";
+    this.insertClasses();
+  }
+
+  isSpecial: boolean = true;
+  color: string = "";
+
+  myClasses:any = {};
+
+  styles = {
+    "font-size" : "20px",
+    "border" : "2px solid blue"
+  }
+
 
   user: User = {
     name: "ohad",
     email: "ohad@mail.com",
     birthDate: Date.parse('1994-09-10'),
-    contact : "Eail",
+    contact: "Eail",
   }
   ngOnInit(): void {
   }
 
-  getAge(dob:number){
-    if(dob != undefined){
-      return (Date.now() - dob )/1000/60 /60 /24 / 365;
-    }else return 
+  getAge(dob: number) {
+    if (dob != undefined) {
+      return (Date.now() - dob) / 1000 / 60 / 60 / 24 / 365;
+    } else return
+  }
+
+  insertClasses(){
+    this.myClasses = {
+      "style1" : true,
+      "style2" : true,
+    }
   }
 }
